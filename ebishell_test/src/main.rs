de5080051ff13c9e1d5a0394575caff6f1ab70d2
vaishnav_sabari_girish <forgamesonly684@gmail.com>
2025-04-18 21:01:36 +0530

    Added Basic commands execution capabilities

--- ebishell_test/src/main.rs
+++ ebishell_test/src/main.rs
@@ -1,4 +1,5 @@
 use std::io::{self, Write};
+use std::process::Command;
 
 fn main() {
     loop {
@@ -8,10 +9,21 @@ fn main() {
         match io::stdin().read_line(&mut input) {
             Ok(0) => break, 
             Ok(_) => {
-                if input.to_lowercase().trim() == "quit" || input.to_lowercase().trim() == "exit" {
+                let mut parts = input.trim().split_whitespace();
+                let command = parts.next().unwrap_or("");
+                if command == "quit" || command == "exit" {
                     break;
                 }
+
+                let output = Command::new(command)
+                    .args(parts)
+                    .output()
+                    .expect("Failed to execute command");
+
+                io::stdout().write_all(&output.stdout).unwrap();
+                io::stderr().write_all(&output.stderr).unwrap();
             },
+
             Err(e) => println!("Error Reading input: {}", e),
         }
     }
